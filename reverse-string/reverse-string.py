ae62a3dc29f5833ecec90b61fbd403a90384d43d
Michael Quansah Hayford <98067149+Michael-600@users.noreply.github.com>
2024-07-30 23:50:51 -0700

    Time: 163 ms, Memory: 20.8 MB - LeetHub

---
+++ reverse-string/reverse-string.py
@@ -0,0 +1,17 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        ptr1 = 0
+        ptr2 = len(s) - 1
+        
+        while ptr1 < ptr2 :
+            letter = s[ptr1]
+            s[ptr1] = s[ptr2]
+            s[ptr2] = letter
+            
+            ptr1 += 1
+            ptr2 -= 1
+        
+        
